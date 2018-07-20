96d7aa181fb6d1f45d8ea68e19ebd00633b30072
illusioninlost <rchang9999@yahoo.com>
2018-07-20 14:13:37 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -25,8 +25,13 @@ Order.defaultProps = {
 
 
 Order.propTypes = {
+    
     cone: PropTypes.bool,
     size: PropTypes.string
     scoops: PropTypes.arrayOf(PropTypes.string).isRequired
-    orderInfo: PropTypes.object.isRequired
+    orderInfo: PropTypes.shape({
+        customerName: PropTypes.string.isRequired,
+        orderedAt: PropTypes.number.isRequired
+    }).isRequired
+
 };
